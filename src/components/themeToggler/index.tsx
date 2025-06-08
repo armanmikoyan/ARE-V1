b1ce275e86c684850c95d2d8bfb95ddd24b33c89
armanmikoyan <arman.mikoyan1@gmail.com>
2025-06-08 17:35:28 +0400

    add audio

--- src/components/themeToggler/index.tsx
+++ src/components/themeToggler/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import WbIncandescentOutlinedIcon from '@mui/icons-material/WbIncandescentOutlined';
 import TungstenIcon from '@mui/icons-material/Tungsten';
 import { Toggler } from './styles';
@@ -9,14 +9,24 @@ import { ThemeMode } from '@/globals/types';
 function ThemeToggle() {
 	const [globalTheme, setGlobalTheme] = useAtom(globalThemeAtom);
 	const isDark: boolean = globalTheme === ThemeMode.DARK;
+	const toggleSound = useRef<HTMLAudioElement | null>(null);
+
+	const toggleTheme = () => {
+		setGlobalTheme((prev) => (prev === ThemeMode.DARK ? ThemeMode.LIGHT : ThemeMode.DARK));
+
+		if (toggleSound.current) {
+			toggleSound.current.currentTime = 0;
+			toggleSound.current.play();
+		}
+	};
 
 	useEffect(() => {
 		document.documentElement.classList.toggle(ThemeMode.DARK, isDark);
 	}, [isDark]);
 
-	const toggleTheme = () => {
-		setGlobalTheme((prev) => (prev === ThemeMode.DARK ? ThemeMode.LIGHT : ThemeMode.DARK));
-	};
+	useEffect(() => {
+		toggleSound.current = new Audio('./themeModeSound.mp3');
+	}, []);
 
 	return (
 		<Toggler onClick={toggleTheme}>
