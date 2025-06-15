0638dddf4e815217c89fdcc8c94711a7453034b2
armanmikoyan <arman.mikoyan1@gmail.com>
2025-06-15 20:14:11 +0400

    test

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 	reactStrictMode: true,
+
+	/// remove this
+	output: 'export',
+	basePath: `/ARE-V1`,
+	assetPrefix: `/ARE-V1/`,
+	images: {
+		unoptimized: true,
+	},
+	///
 };
 
 export default nextConfig;
