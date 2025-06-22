e453b7391d1fc7ba7cae205e866bd17fb932a225
armanmikoyan <arman.mikoyan1@gmail.com>
2025-06-22 19:29:15 +0400

    test deployemnt

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,26 @@
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+// 	/* config options here */
+// 	reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+// to remove for github pages deployement
 import type { NextConfig } from 'next';
 
+const repoName = 'ARE-V1';
+
 const nextConfig: NextConfig = {
-	/* config options here */
 	reactStrictMode: true,
+	output: 'export',
+	distDir: 'out',
+	assetPrefix: `/${repoName}/`,
+	basePath: `/${repoName}`,
+	env: {
+		NEXT_PUBLIC_BASE_PATH: `/${repoName}`,
+	},
 };
 
 export default nextConfig;
