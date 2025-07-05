ca880d88f57d2f9ccd2cc690a9e2edc73b21cfb4
armanmikoyan <arman.mikoyan1@gmail.com>
2025-07-05 22:45:21 +0400

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from 'next';
 
+// const nextConfig: NextConfig = {
+// 	reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-	reactStrictMode: true,
+	output: 'export',
+	images: {
+		unoptimized: true,
+	},
+	basePath: '/are-v1',
+	assetPrefix: '/are-v1',
 };
 
 export default nextConfig;
